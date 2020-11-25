e459f82ce8a69686cd69f64858e48f9a9b68e069
wangcheng <aa19910101>
2020-11-25 17:58:40 +0800

    新建流程项目

---
+++ springboot-activiti/src/test/java/com/cest/Part1_DeploymentTest.java
@@ -0,0 +1,88 @@
+package com.cest;
+
+import lombok.extern.slf4j.Slf4j;
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.repository.Deployment;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.InputStream;
+import java.util.List;
+import java.util.zip.ZipInputStream;
+
+/**
+ * 流程部署测试
+ */
+@Slf4j
+public class Part1_DeploymentTest extends BaseTest {
+
+    @Autowired
+    private RepositoryService repositoryService;
+
+    @Test
+    public void testHello() {
+        log.info("hello");
+    }
+
+
+    //通过bpmn部署流程
+    @Test
+    public void initDeploymentByBPMN() {
+        String filename="BPMN/Part1_Deployment.bpmn";
+        // String pngname="BPMN/Part1_Deployment.png";
+        Deployment deployment = repositoryService.createDeployment()
+                .addClasspathResource(filename)
+                //.addClasspathResource(pngname)//图片
+                .name("流程部署测试申请task")
+                .deploy();
+        System.out.println(deployment.getName());
+    }
+
+
+    //通过zip部署流程
+    @Test
+    public void initDeploymentByZip() {
+        String filename="BPMN/Part1_DeploymentV2.zip";
+
+        InputStream resourceAsStream = this.getClass()
+                .getClassLoader()
+                .getResourceAsStream(filename);
+
+        ZipInputStream zipInputStream = new ZipInputStream(resourceAsStream);
+
+        Deployment deployment = repositoryService.createDeployment()
+                .addZipInputStream(zipInputStream)
+                .name("流程部署zip")
+                .deploy();
+
+        System.out.println(deployment.getName());
+    }
+
+    //部署流程查询 -- 集合
+    @Test
+    public void getDeploymentWithList() {
+        List<Deployment> list = repositoryService.createDeploymentQuery().list();
+
+        for(Deployment dep : list){
+            log.info("Id："+dep.getId());
+            log.info("Name："+dep.getName());
+            log.info("DeploymentTime："+dep.getDeploymentTime());
+            log.info("Key："+dep.getKey());
+        }
+    }
+
+    //部署流程查询 -- 条件查询
+    @Test
+    public void getDeploymentByCondition() {
+        List<Deployment> list = repositoryService.createDeploymentQuery()
+                .deploymentId("b050f43c-2ef6-11eb-a33f-00ff56d6be42")
+                .list();
+
+        for(Deployment dep : list){
+            log.info("Id："+dep.getId());
+            log.info("Name："+dep.getName());
+            log.info("DeploymentTime："+dep.getDeploymentTime());
+            log.info("Key："+dep.getKey());
+        }
+    }
+}
---
+++ springboot-activiti/src/test/java/com/cest/Part2_ProcessDefinitionTest.java
@@ -0,0 +1,10 @@
+package com.cest;
+
+import org.activiti.engine.RepositoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Part2_ProcessDefinitionTest extends BaseTest {
+
+    @Autowired
+    private RepositoryService repositoryService;
+}
